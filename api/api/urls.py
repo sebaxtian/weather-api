f270d57ca764a124b4ea666fd5c82e289ee31a56
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2021-06-04 16:00:00 -0500

    Setup JWT URLs

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token-auth/', obtain_jwt_token),
+    path('api/token-refresh/', refresh_jwt_token),
+    path('api/token-verify/', verify_jwt_token),
 ]
