ec802cee9616cbaeaf33f54b1e57712a4daa1a81
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2021-06-04 15:51:57 -0500

    Setup WSGI settings by .env file

--- api/api/wsgi.py
+++ api/api/wsgi.py
@@ -11,6 +11,21 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+from dotenv import load_dotenv
+from pathlib import Path
+from shutil import copyfile
+
+# Check dotenv
+if not os.path.exists(Path('../') / '.env'):
+    copyfile(Path('../') / 'example.env', Path('../') / '.env')
+# Load dotenv
+load_dotenv(dotenv_path=Path('../') / '.env')
+
+
+if os.getenv('DJANGO_SETTINGS_MODULE') is not None:
+    DJANGO_SETTINGS_MODULE = os.getenv('DJANGO_SETTINGS_MODULE')
+else:
+    DJANGO_SETTINGS_MODULE = 'api.settings.prod'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", DJANGO_SETTINGS_MODULE)
 
 application = get_wsgi_application()
